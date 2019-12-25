c06ca998e219a1b00bd5e1c69fd962611e44a67a
dizzy-why <regeshmaharjan2015@gmail.com>
2019-12-25 13:07:53 +0900

    minor changes

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index_page():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = '8080')
+    app.run(threaded=True,port = 8080)
